60cd6adc257b36fc9fb65e16aeb2b1409803df13
Iheeb011 <162156874+Iheeb011@users.noreply.github.com>
2024-03-27 22:43:40 +0100

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -5,5 +5,12 @@ function toggleMenu() {
     icon.classList.toggle("open");
     
   };
+document.addEventListener("DOMContentLoaded", function() {
+    const prj = document.getElementById("prj1");
+    prj.addEventListener('click', function() {
+        window.location.href = "https://iheeb011.github.io/Cash-Register/";
+    });
+});
+
   
   
